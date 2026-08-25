43e6047fd27d3c7d55f235d680f734f2db4fe745
Ben <41802624+benlewisss@users.noreply.github.com>
2026-08-25 23:56:19 +0100

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        # Logic is: The number of unique paths from any particular location is simply the number of
+        # unique paths from each adjacent square summed. This is recursive, and because if we map
+        # out the decision tree, many paths are replicated, we can simply cache them.
+
+        unique_path_cache = dict()
+        
+        def unique_paths(row: int, col: int) -> int:
+            # Only bounds checking in one direction because we only move down and right
+            if (row >= m or col >= n):
+                return 0
+            
+            # Reached target, theres only 1 path from here because we're already here
+            if (row == m - 1 and col == n - 1):
+                return 1
+
+            if (row, col) in unique_path_cache:
+                return unique_path_cache[(row, col)]
+
+            unique_path_cache[(row, col)] = unique_paths(row + 1, col) + unique_paths(row, col + 1)
+            return unique_path_cache[(row, col)]
+
+        return unique_paths(0, 0)
