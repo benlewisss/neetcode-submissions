e6cc2d3b5612ef090bf9eff6a3eba5493717deb8
Ben <41802624+benlewisss@users.noreply.github.com>
2026-09-03 14:16:11 +0100

    Add: range-sum-query-2d-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-2.py
@@ -0,0 +1,41 @@
+import copy
+import pprint
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        
+        # This is the same as the Viola-Jones integral image I learned about in uni Comp Vis
+        # Because it's a 2D grid, each "prefix" in a prefix-sum table musn't just be the sum of all numbers
+        # to it's left, it's the sum of all number's above and to the left of it (in 2D, not 1D).
+
+        self.matrix = matrix
+        self.summed_area_table = copy.deepcopy(matrix)
+
+
+        # To populate prefix sum: for any val [i][j], sum is [i][j] + [i-1][j] + [i][j-1] - [i-1][j-1]
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+
+        for i in range(num_rows):
+            for j in range(num_cols):
+                north_val = (self.summed_area_table[i - 1][j] if i >= 1 else 0)
+                west_val = (self.summed_area_table[i][j - 1] if j >= 1 else 0)
+                north_west_val = (self.summed_area_table[i - 1][j - 1] if (i >= 1 and j >= 1) else 0)
+                self.summed_area_table[i][j] = matrix[i][j] + north_val + west_val - north_west_val
+
+        pprint.pp(self.summed_area_table)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+
+        north_val = (self.summed_area_table[row1 - 1][col2] if row1 >= 1 else 0)
+        west_val = (self.summed_area_table[row2][col1 - 1] if col1 >= 1 else 0)
+        north_west_val = (self.summed_area_table[row1 - 1][col1 - 1] if (row1 >= 1 and col1 >= 1) else 0)
+
+        return (self.summed_area_table[row2][col2] - north_val - west_val + north_west_val)
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
