5aa31be1cb2f421f44ccc40f8a8ac429cec88194
Ben <41802624+benlewisss@users.noreply.github.com>
2026-09-12 19:18:57 +0100

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        
+        # After reading and understanding question, the actions taken can be generalised 
+        #in the formula [0, n-1, 1, n-2, 2, n-3, ...] from nodes 0 to n-1. 
+        # So, essentially what this is:
+        # - Split the linked list into two halfs (the second half being shorter if length is odd)
+        # - Reverse the second half
+        # - Insert it after every node in the first half. Bam. Done.
+
+        # Edge cases
+        if not head.next:
+            return
+
+        # Step 1: Find middle
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = ListNode(slow.next.val, slow.next.next)
+        slow.next = None
+
+        # Step 2: Reverse second half
+
+        prev = None
+        curr = second_half
+        while curr:
+            tmp = ListNode(curr.val, curr.next)
+            curr.next = prev
+            prev = curr
+            curr = tmp.next
+
+        reversed_second_half = prev
+        
+        # Step 3: Combine
+
+        curr1 = head
+        curr2 = reversed_second_half
+        while curr2 and curr1:
+            tmp1 = ListNode(curr1.val, curr1.next)
+            tmp2 = ListNode(curr2.val, curr2.next)
+
+            curr1.next = curr2
+            curr2.next = tmp1.next
+
+            curr1 = tmp1.next
+            curr2 = tmp2.next
+        
+ 
+
