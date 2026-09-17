b145c6e1058a22df92540dfa2c7dba73068b4d96
Ben <41802624+benlewisss@users.noreply.github.com>
2026-09-17 19:39:12 +0100

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dialpad = {'2': {'a','b','c'}, '3': {'d','e','f'}, '4': {'g','h','i'}, '5': {'j','k','l'}, '6': {'m','n','o'}, '7': {'p','q','r','s'}, '8': {'t','u','v'}, '9': {'w','x','y','z'}}
+        all_combinations = []
+
+        def backtrack(index, curr_string):
+            if len(curr_string) == len(digits):
+                all_combinations.append(curr_string)
+                return
+
+            if index >= len(digits):
+                return
+
+            for char in dialpad[digits[index]]:
+                backtrack(index + 1, curr_string + char)
+
+        backtrack(0, "")
+        if len(all_combinations) == 1 and all_combinations[0] == "":
+            all_combinations = []
+        return all_combinations
+
+            
