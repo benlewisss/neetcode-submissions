ad900d1c1fa12144c586a3c3121bfc247c46d5ac
Ben <41802624+benlewisss@users.noreply.github.com>
2026-09-26 14:43:00 +0100

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        memo = {}
+        def backtrack(pointer):
+            if pointer >= len(s):
+                return True
+
+            if (pointer not in memo):
+                res = False
+
+                for word in wordDict:
+                    if s[pointer:pointer+len(word)] == word:
+                        res = res or backtrack(pointer+len(word))
+
+                memo[pointer] = res
+
+            return memo[pointer]
+
+        return backtrack(0)
